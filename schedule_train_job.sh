17cfef8851124e3f3c4a26be5511bbf708373c23
Mitry Mark Anderson <mitryand@gl-login2.arc-ts.umich.edu>
2022-11-16 12:19:21 -0500

    setup for greatlakes

---
+++ schedule_train_job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --partition=spgpu
+#SBATCH --time=00-02:20:00
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=4
+#SBATCH --mem-per-gpu=32GB
+#SBATCH --job-name=mitryand_EECS595_project_train
+#SBATCH --account=eecs595f22_class
+#SBATCH --output=/home/%u/EECS-595-Final-Project-Style-Transfer/outputs/%x-%j.log
+#SBATCH --mail-user=mitryand@umich.edu
+
+# set up job
+module load python/3.10.4 
+module load cuda
+# pushd /home/kpyu/great-lakes-tutorial
+source venv/bin/activate
+
+# run job
+python train_model.py --batch_size=16 --num_epochs=12
