8de86da108266548273aae564db25058f9b3a6a1
mitryand <mitryand@umich.edu>
2022-11-17 12:42:56 -0500

    big brain

--- train_model.py
+++ train_model.py
@@ -111,9 +111,9 @@ def train(model, train_dataloader, eval_dataloader, params, input_tokenizer, out
             pred = []
             truth = []
             print(output_tokenizer.batch_decode(outputs))
-            for i in range(len(outputs)):
-                pred.append(output_tokenizer.decode(outputs[i], skip_special_tokens=True))
-                truth.append(input_tokenizer.decode(batch[0][i], skip_special_tokens=True))
+            pred.append(output_tokenizer.decode(outputs, skip_special_tokens=True))
+            truth.append(input_tokenizer.decode(batch[0], skip_special_tokens=True))
+
             metric.add_batch(predictions=pred, references=truth)
 
             print("===========================")
