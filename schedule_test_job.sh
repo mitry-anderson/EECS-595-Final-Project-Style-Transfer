1283a46eca9b3065e69a53d81ef1d11618c2eafd
Mitry Mark Anderson <mitryand@gl-login1.arc-ts.umich.edu>
2022-11-17 12:03:02 -0500

    hmm

---
+++ schedule_test_job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --partition=spgpu
+#SBATCH --time=00-01:00:00
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=4
+#SBATCH --mem-per-gpu=32GB
+#SBATCH --job-name=mitryand_EECS595_project_train
+#SBATCH --account=eecs595f22_class
+#SBATCH --output=/home/%u/EECS-595-Final-Project-Style-Transfer/outputs/%x-%j.log
+#SBATCH --mail-user=mitryand@umich.edu
+
+# set up job
+module load python/3.10.4 
+module load cuda
+
+source venv/bin/activate
+
+# run job
+python train_model.py --test=True --model_name=news_adventure.torch
